24d17e59530762705d940fc68c9e0af38be40638
nicklassaloyce-debug <nicklassaloyce@gmail.com>
2026-04-24 15:52:03 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "John";
+        int age = 21;
+        String country = "Tanzania";
+
+        System.out.println("My Information");
+        System.out.println("My name is " + name);
+        System.out.println("I am " + age + " years old");
+        System.out.println("I live in " + country);
+        System.out.println("I am learning Java programming");
+    }
+}Main.class
